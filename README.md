c63aacf36349f164c67dd31d79d8580555ca4466
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-11-05 06:49:12 +0100

    Setup: Linters

---
+++ README.md
@@ -0,0 +1,94 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Awesome Books
+
+![](./hello.png)
+
+## Project Details
+
+> This is a project created using HTML, CSS and JavaScript to show my knowledge of them.
+
+## Built With
+
+- HTML
+- CSS
+- Javascript
+
+## Additional Tools
+
+- WebHint
+- Stylelint
+- Node Packet Manager
+- Github Pages
+
+
+## Getting Started
+
+set up a new repository and prepare it for development using best practices (e.g. linters).
+build the first required sections of the website using the template you chose in the previous step.
+
+### Prerequisites
+
+- HTML and CSS and Basic JavaScript.
+- Git installation
+- A text editor 
+- A terminal
+- A web browser to view output
+- An IDE e.g Visual studio code
+
+### Video link
+
+
+### Live Demo
+
+[Live Demo Link](################)
+
+
+### Setup
+
+- Clone this repository or download the Zip folder:
+
+
+for https:
+```
+    git clone https://github.com/lukman155/htmlcsscapstone.git
+```
+for ssh:
+```
+    git clone git@github.com:lukman155/htmlcsscapstone.git
+
+```
+
+### Usage
+
+- After Cloning this repo to your local machine
+- Open the `index.html` in your browser
+
+## Authors
+
+👤 **Lukman Abdulkarim**
+
+- GitHub: [@lukman155](https://github.com/lukman155)
+- Twitter: [@LukmanAbdulka18](https://twitter.com/LukmanAbdulka18)
+- LinkedIn: [Lukman Abdulkarim](https://linkedin.com/in/lukmanbaba)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/lukman155/@@@@/issues).
+
+## Show your support
+
+Give two ⭐️'s if you like this project!
+
+## Acknowledgments
+
+- Thanks to the Microverse team for the great curriculum.
+- Thanks to the Code Reviewer(s) for the insightful feedback.
+- A great thanks to My coding partner(s), morning session team, and standup team for their contributions.
+- Hat tip to anyone whose code was used.
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- src/index.js
+++ src/index.js
@@ -1,36 +1,28 @@
-import './style.css'
+import './style.css';
 
 const todoList = document.querySelector('.todo-list');
-const form = document.querySelector('.form')
-const taskInput = document.querySelector('.task-input')
+const form = document.querySelector('.form');
+const taskInput = document.querySelector('.task-input');
 
-let tasks = [{description:'Pls Work', completed:false, index:0}, 
-            {description:'Pele Work', completed:false, index:1}]
+const tasks = JSON.parse(localStorage.getItem('task.lists')) || [];
 
 class NewTask {
-    constructor(description, index) {
-        this.description = description;
-        this.completed = false;
-        this.index = index;
-    }
+  constructor(description, index) {
+    this.description = description;
+    this.completed = false;
+    this.index = index;
+  }
 }
-            
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    let taskDesc = taskInput.value
-    let index = tasks.length
-    if (taskDesc == null || taskDesc === '') return
-    const createTask = new NewTask(taskDesc, index)
-    tasks.push(createTask)
-    taskInput.value = null
-    render()
-})
-
+const clearList = (list) => {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+};
 
 const render = () => {
-    clearList(todoList);
-    tasks.forEach((task) => {
+  clearList(todoList);
+  tasks.forEach((task) => {
     const listItem = document.createElement('li');
     listItem.classList.add('list-item');
     todoList.appendChild(listItem);
@@ -39,30 +31,43 @@ const render = () => {
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    checkbox.setAttribute('id', `${task.index}`)
+    checkbox.setAttribute('id', `${task.index}`);
     checkbox.classList.add('task-status');
     div.appendChild(checkbox);
 
     const label = document.createElement('label');
     label.classList.add('task-desc');
     label.textContent = task.description;
-    label.setAttribute('for', `${task.index}`)
+    label.setAttribute('for', `${task.index}`);
     div.appendChild(label);
 
-    const button = document.createElement('button')
-    button.innerHTML = '&#8942;'
-    button.classList.add('more-icon')
-
+    const button = document.createElement('button');
+    button.innerHTML = '&#8942;';
+    button.classList.add('more-icon');
 
     listItem.appendChild(div);
-    listItem.appendChild(button)
-    })
-}
+    listItem.appendChild(button);
+  });
+};
 
+const save = () => {
+  localStorage.setItem('task.lists', JSON.stringify(tasks));
+};
 
-const clearList = list => {
-    while (list.firstChild) {
-        list.removeChild(list.firstChild)
-    }
-}
-render()
+const saveRender = () => {
+  save();
+  render();
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const taskDesc = taskInput.value;
+  const index = tasks.length;
+  if (taskDesc == null || taskDesc === '') return;
+  const createTask = new NewTask(taskDesc, index);
+  tasks.push(createTask);
+  taskInput.value = null;
+  saveRender();
+});
+
+render();
