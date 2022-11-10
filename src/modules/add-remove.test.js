48882e8660107f33e356df7cc232b3c5235df3cd
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-11-10 20:35:38 +0100

    Update add-remove.test.js

--- src/modules/add-remove.test.js
+++ src/modules/add-remove.test.js
@@ -1,22 +1,74 @@
 /** @jest-environment jsdom */
-import render from './add-remove.js';
+import render, { removeTodo } from './add-remove.js';
 import '@testing-library/jest-dom';
-// import { save } from './storage.js';
-// import { save, tasks } from './storage.js';
 
-beforeEach(() => {
-  document.body.innerHTML = `
-    <ul class='todo-List'></ul>`;
-  const tasks = [{
-    description: 'test',
-    completed: false,
-    index: 1,
-  }];
-  render(tasks);
+describe('render', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <ul class='todo-List'></ul>`;
+    const tasks = [{
+      description: 'test',
+      completed: false,
+      index: 1,
+    }];
+    render(tasks);
+  });
+  test('Check addTodo able add todo to todoList', () => {
+    const todoList = document.querySelector('.todo-List');
+
+    expect(todoList.children.length).toBe(1);
+  });
 });
 
-test('Check addTodo able add todo to todoList', () => {
-  const todoList = document.querySelector('.todo-List');
+describe('render', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <ul class='todo-List'></ul>`;
+    const tasks = [{
+      description: 'test',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'test',
+      completed: false,
+      index: 2,
+    }];
+    render(tasks);
+  });
 
-  expect(todoList.children.length).toBe(1);
+  test('Check addTodo', () => {
+    const todoList = document.querySelector('.todo-List');
+
+    expect(todoList.children.length).toBe(2);
+  });
+});
+
+describe('removeTodo', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <ul class='todo-List'>
+        <li class='list-item'>
+          <div>
+            <input type='checkbox' id='1' class='task-status'>
+            <label class='task-desc' for='1'>test</label>
+          </div>
+          <button class='more-icon'>&#8942;</button>
+          <button class='more-icon del-btn disappear'>&#128465;</button>
+        </li>
+      </ul>`;
+    const button = document.querySelector('.more-icon');
+    const tasks = [{
+      description: 'test',
+      completed: false,
+      index: 1,
+    }];
+    removeTodo(button, tasks[0]);
+  });
+
+  test('Check removeTodo able remove todo from todoList', () => {
+    const todoList = document.querySelector('.todo-List');
+
+    expect(todoList.children.length).toBe(0);
+  });
 });
