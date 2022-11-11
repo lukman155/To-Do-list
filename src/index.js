60141a9c2b7dfa94c1f8a8e63f3796e7085346ff
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-11-11 16:40:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import NewTask from './modules/task.js';
 import { save, tasks } from './modules/storage.js';
 import {
   todoList, form, taskInput, clearButton,
-} from './modules/interactive.js';
+} from './modules/elements.js';
 import render, { saveRender } from './modules/render.js';
 // Add Task
 
